cfaa831e319871290d4c2bd4131717f001bda724
Ricardo <rickyantonio117@gmail.com>
2023-12-19 12:24:31 -0800

    Add header and footer

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+    get_header();
 
     while(have_posts()) {
         the_post(); ?>
@@ -7,4 +8,5 @@
         <hr>
     <?php }
 
+    get_footer();
 ?>
--- page.php
+++ page.php
@@ -1,4 +1,5 @@
 <?php
+    get_header();
 
     while(have_posts()) {
         the_post(); ?>
@@ -6,4 +7,5 @@
         <p><?php the_content(); ?></p>
     <?php }
 
+    get_footer();
 ?>
