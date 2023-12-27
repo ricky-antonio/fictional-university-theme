1f0037c2645fd94fac8f6137b0fc0e9614da0d9e
Ricardo <rickyantonio117@gmail.com>
2023-12-27 11:53:03 -0800

    Page banner function

--- single-professor.php
+++ single-professor.php
@@ -1,20 +1,9 @@
 <?php
     get_header();
-    $pageBannerImage = get_field('page_banner_background_image');
+    pageBanner();
     
     while(have_posts()) {
         the_post(); ?>
-        <div class="page-banner">
-            <div class="page-banner__bg-image" 
-            style="background-image: url(<?php echo $pageBannerImage['sizes']['pageBanner'] ?>)">
-            </div>
-            <div class="page-banner__content container container--narrow">
-                <h1 class="page-banner__title"><?php the_title(); ?></h1>
-                <div class="page-banner__intro">
-                    <p><?php the_field('page_banner_subtitle'); ?></p>
-                </div>
-            </div>
-        </div>
 
         <div class="container container--narrow page-section">
             
